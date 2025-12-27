3fd74062bea6b7e6deddb6e9ec7ae0d2e111b731
Ankit Lenka <ankitlenka121@gmail.com>
2025-12-27 14:14:40 +0530

    new commit

--- src/com.ankit.dsa/Palindrome.java
+++ src/com.ankit.dsa/Palindrome.java
@@ -1,4 +1,26 @@
 package com.ankit.dsa;
 
+import java.util.Scanner;
+
 public class Palindrome {
-}
+
+
+
+        public static void main (String[] args) {
+            Scanner org=new Scanner(System.in);
+            int x=org.nextInt();
+            int rev=0;
+            int sum=0;
+            int i=x;
+            while(x!=0) {
+                rev = x % 10;
+                x = x / 10;
+                sum = sum * 10 + rev;
+            }
+            if (sum==i)
+                System.out.println("pallendrome");
+            else
+                System.out.println(" not pallendrome");
+        }
+    }
+
